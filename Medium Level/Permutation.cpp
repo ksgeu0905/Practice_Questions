acd3f101a4da5570aac7682125001364bd14d8d7
Kartikya Singhal <Kartikyasinghalgeu@gmail.com>
2022-05-09 00:37:22 +0530

    Linked List

---
+++ Medium Level/Permutation.cpp
@@ -0,0 +1,19 @@
+/*
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        do {        
+            ans.push_back(nums);
+        }while (next_permutation(nums.begin(), nums.end()));
+        return ans;
+    }
+};
+
+*/
