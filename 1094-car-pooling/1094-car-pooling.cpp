d5eaf88d6108682decb2adc33ce8299e3edbe115
ksgeu0905 <91470947+ksgeu0905@users.noreply.github.com>
2022-05-24 15:55:10 +0530

    Time: 35 ms (21.63%), Space: 11.1 MB (20.36%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+         priority_queue<int,vector<int>,greater<int>> pick_up,drop_off;
+        map<int,int>pick,drop;
+        int last_station=INT_MIN;
+        for(int i=0;i<trips.size();i++){
+            last_station=max(last_station,trips[i][2]);
+            pick_up.push(trips[i][1]);
+            drop_off.push(trips[i][2]);
+            pick[trips[i][1]]+=trips[i][0];
+            drop[trips[i][2]]+=trips[i][0];
+        }
+        
+        int passenger=0;
+        for(int i=0;i<=last_station;i++){
+            if(!pick_up.empty() && i==pick_up.top()){
+                passenger+=pick[i];
+                while(!pick_up.empty() && i==pick_up.top())
+                    pick_up.pop();
+            }
+             if(!drop_off.empty() && i==drop_off.top()){
+                passenger-=drop[i];
+                while(!drop_off.empty() && i==drop_off.top())
+                    drop_off.pop();
+            }
+            if(passenger>capacity)
+                return false;
+            
+        }
+        return true;
+        
+    }
+};
