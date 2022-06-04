645fa0a7626aa40036e6395cecb3dd189fb4b13b
ksgeu0905 <91470947+ksgeu0905@users.noreply.github.com>
2022-06-04 22:05:20 +0530

    Time: 3 ms (54.08%), Space: 8 MB (16.90%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -21,8 +21,6 @@ public:
         for(int i=1;i<nums.size();i++){
             v2.push_back(nums[i]);
         }
-        // int a=fun(v1);
-        // int b=fun(v2);
         return max(fun(v1),fun(v2));
     }
 };
