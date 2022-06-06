09f97c0e9f448ae37e0f2afef9984080654bc476
ksgeu0905 <91470947+ksgeu0905@users.noreply.github.com>
2022-06-06 23:21:38 +0530

    Time: 7 ms (34.00%), Space: 7.8 MB (63.23%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    int find(int m,int n,vector<vector<int>> & dp, vector<vector<int>> ogrid){
+   int func(vector<vector<int>>&grid,int i, int j, vector<vector<int>>&dp)
+    {
+        if(i==0 && j == 0 && grid[i][j]!=1)return 1;
+        if(i<0 || j<0) return 0;
+        if(grid[i][j] == 1)return 0;
+        if(dp[i][j]!= -1) return dp[i][j];
         
-        if(m==0 && n==0 && ogrid[m][n]!=1)
-            return 1;
-        if(m<0 || n<0)
-            return 0;
-        if(ogrid[m][n]==1)
-            return 0;
-        if(dp[m][n]!= -1 )
-            return dp[m][n];
+        int u = func(grid, i-1,j,dp);
+        int p = func(grid, i,j-1,dp);
         
-        int up=find(m-1,n,dp,ogrid);
-        int left=find(m,n-1,dp,ogrid);
+        return dp[i][j] = u+p;
         
-        return dp[m][n]=up+left;
     }
-    int uniquePathsWithObstacles(vector<vector<int>>& ogrid) {
-        int m=ogrid.size();
-        int n=ogrid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return find(m-1,n-1,dp,ogrid);
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n =  obstacleGrid[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return func(obstacleGrid,m-1,n-1,dp);
     }
 };
