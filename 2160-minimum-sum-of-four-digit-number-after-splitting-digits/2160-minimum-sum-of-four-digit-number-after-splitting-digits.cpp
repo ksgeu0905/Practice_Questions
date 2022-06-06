eb3df2779f82058f586d3593430b35d0502a12f8
ksgeu0905 <91470947+ksgeu0905@users.noreply.github.com>
2022-06-07 00:03:20 +0530

    Time: 2 ms (40.73%), Space: 6 MB (26.28%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -1,25 +1,10 @@
 class Solution {
 public:
     int minimumSum(int num) {
-        string str=to_string(num);
-        sort(str.begin(),str.end());
-        string str1="",str2="";
-        for(int i=0;i<4;i++){
-            if(i%2==0)
-                str1+=str[i];
-            else
-                str2+=str[i];
-        }
-        
-        stringstream geek(str1);
- 
-        int x = 0,y=0;
-        geek >> x;
-        
-        stringstream geek2(str2);
-        geek2>>y;
-
-        
+       string s = to_string(num);
+        sort(s.begin(), s.end());
+        int x = (s[0] - '0') * 10 + s[2] - '0';
+        int y = (s[1] - '0') * 10 + s[3] - '0';
         return x+y;
     }
 };
