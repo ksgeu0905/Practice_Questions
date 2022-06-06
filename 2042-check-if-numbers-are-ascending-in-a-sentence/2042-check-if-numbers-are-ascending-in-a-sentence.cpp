fc6f70a9ad63a1e35b90a1643d5e675bf2bd8bf8
ksgeu0905 <91470947+ksgeu0905@users.noreply.github.com>
2022-06-07 00:47:44 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (88.74%) - LeetHub

--- 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.cpp
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
     bool areNumbersAscending(string s) {
-        vector<int> v,v2;
-        string str="";
-        for(int i=0 ; i< s.length();i++){
-            if(s[i]>='0' && s[i]<='9' && (s[i+1]=='\0' || s[i+1]==' ')){
-                v.push_back(s[i]-'0');
+        int prev=INT_MIN;
+        int flag = 0;
+        for(int i = 0 ; i<s.length();i++)
+        {
+            if(s[i]>='0' && s[i]<='9' &&(s[i+1]=='\0' || s[i+1]==' '))
+            {
+                int x = s[i]-'0';
+                if(prev<x) prev = x;
+                else if(prev>=x)
+                {
+                    flag = 1;
+                    break;
+                }
             }
-            else if(s[i]>='0' && s[i]<='9' && (s[i+1]>='0' && s[i+1]<='9')){
-                v.push_back((s[i]-'0')*10 + s[i+1]-'0');
+            else  if(s[i]>='0' && s[i]<='9' && s[i+1]!=' ') 
+            {
+                int x = (s[i]-'0' )*10 +s[i+1]-'0';
                 i++;
+                if(prev<x)
+                    prev=x;
+                else if(prev>=x)
+                {
+                    flag = 1;
+                    break;
+                }
             }
+            
         }
-        v2=v;
-        sort(v2.begin() ,v2.end());
-        for(int i=0;i<v.size()-1;i++){
-            if(v2[i]==v2[i+1])return false;
-        }
-        return v2==v;
+        if(flag==1)
+            return false;
+        return true;
     }
+    
 };
