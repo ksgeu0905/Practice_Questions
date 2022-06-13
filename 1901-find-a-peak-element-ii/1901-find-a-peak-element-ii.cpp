3deac9a01be0a56b844403d2c996822f1b92c820
ksgeu0905 <91470947+ksgeu0905@users.noreply.github.com>
2022-06-13 19:38:58 +0530

    Time: 313 ms (14.13%), Space: 46.2 MB (76.35%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
-        int j=0;
-        int mx=-1;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>mx){
-                j=i;
-                mx=nums[i];
-            }
-        }
-        return j;
-    }
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int i=0,j;
-            vector<int> ans;
-        for(int i=0;i<mat.size();i++){
-            j=findPeakElement(mat[i]);
-            if(i!=mat.size()-1 && mat[i][j]>mat[i+1][j]){
-                ans.push_back(i);
-                ans.push_back(j);
-                return ans;
+        int n = mat.size(), m = mat[0].size();
+        int l = 0, h = n-1;
+   
+        while(l <= h){
+            int i = l+(h-l)/2;  
+        
+            int mx = 0,j = 0;    
+            for(int x = 0; x < m; x++){
+                if(mx < mat[i][x]){
+                    mx = mat[i][x];
+                    j = x;
+                }
             }
-            else if(i==mat.size()-1){
-                ans.push_back(i);
-                ans.push_back(j);
-                return ans;
-            }        
+        
+            int t = (i == 0?-1:mat[i-1][j]); 
+            int b = (i == n-1?-1:mat[i+1][j]);
+        
+            if(mx > t && mx > b){
+                return {i,j};
+            }
+            else if(mx > t && mx < b){
+                l = i+1;
+            }
+            else h = i-1;
         }
-        return ans;
+    
+        return {0,0};
     }
 };
