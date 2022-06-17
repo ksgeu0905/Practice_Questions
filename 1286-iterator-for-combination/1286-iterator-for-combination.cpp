45144145edd33cf9f736302c81fbda74cd9e93fc
ksgeu0905 <91470947+ksgeu0905@users.noreply.github.com>
2022-06-18 01:50:08 +0530

    Time: 19 ms (41.10%), Space: 13.6 MB (8.90%) - LeetHub

--- 1286-iterator-for-combination/1286-iterator-for-combination.cpp
+++ 1286-iterator-for-combination/1286-iterator-for-combination.cpp
@@ -3,15 +3,14 @@ public:
     vector<string> arr;
     int pos = 0;
     
-    void solve(string &s, int len, int pos, string &op){
+    void solve(string &s, int len, int pos, string op){
         if(pos==s.size()){
             if(op.size()==len)
                 arr.push_back(op);
             return;
         }
-        op.push_back(s[pos]);
-        solve(s,len,pos+1,op);
-        op.pop_back();
+       
+        solve(s,len,pos+1,op+s[pos]);
         solve(s,len,pos+1,op);
     }
     
