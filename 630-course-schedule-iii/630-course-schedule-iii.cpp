819f6e49c7260ad100fc1f772fa8d176d1f09b3b
ksgeu0905 <91470947+ksgeu0905@users.noreply.github.com>
2022-06-24 01:24:01 +0530

    Time: 388 ms (42.53%), Space: 63.3 MB (34.25%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    static bool compare(vector<int> a,vector<int>b){
+    static bool compare(vector<int>& a,vector<int>&b){
         return a[1]<b[1];
     }
     int scheduleCourse(vector<vector<int>>& courses) {
