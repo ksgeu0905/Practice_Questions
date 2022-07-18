ae71d852e31c0d8570638cd53f375917feb45d28
ksgeu0905 <91470947+ksgeu0905@users.noreply.github.com>
2022-07-18 22:34:51 +0530

    Time: 155 ms (39.32%), Space: 45.2 MB (23.53%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
-    int find(vector<int> & coins,int ind,int tar,vector<vector<int>> & dp){
-        if(ind==0){
-            if(tar%coins[0]==0)
-                return tar/coins[0];
-            return 1e9;
-        }
-        if(dp[ind][tar]!=-1)return dp[ind][tar]; 
-        int n_pick=find(coins,ind-1,tar,dp);
-        int pick=INT_MAX;
-        if(tar>=coins[ind]){
-            pick=1+find(coins,ind,tar-coins[ind],dp);
-        }
-        return dp[ind][tar]=min(n_pick,pick);
-    }
+    // int find(vector<int> & coins,int ind,int tar,vector<vector<int>> & dp){
+    //     if(ind==0){
+    //         if(tar%coins[0]==0)
+    //             return tar/coins[0];
+    //         return 1e9;
+    //     }
+    //     if(dp[ind][tar]!=-1)return dp[ind][tar]; 
+    //     int n_pick=find(coins,ind-1,tar,dp);
+    //     int pick=INT_MAX;
+    //     if(tar>=coins[ind]){
+    //         pick=1+find(coins,ind,tar-coins[ind],dp);
+    //     }
+    //     return dp[ind][tar]=min(n_pick,pick);
+    // }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
-        int x=find(coins,coins.size()-1,amount,dp);
-        if(x==1e9)
+        int n=coins.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        for(int t=0;t<=amount;t++){
+            if(t%coins[0]==0)
+                dp[0][t]=t/coins[0];
+            else
+                dp[0][t]=1e9;
+        }
+        for(int ind = 1 ;ind<n;ind++){
+            for(int tar=0;tar<=amount;tar++){
+                    int n_pick=dp[ind-1][tar];
+                    int pick=INT_MAX;
+                    if(tar>=coins[ind]){
+                        pick=1+dp[ind][tar-coins[ind]];
+                    }
+                    dp[ind][tar]=min(pick,n_pick);
+            }
+        }
+        int x=dp[n-1][amount];
+        if(x>=1e9)
             return -1;
         else return x;
-    }
+    } 
 };
