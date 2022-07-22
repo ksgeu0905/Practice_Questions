24941abc212418d18b4af08f9aaee387e0ab5c5d
ksgeu0905 <91470947+ksgeu0905@users.noreply.github.com>
2022-07-23 00:02:08 +0530

    Time: 1401 ms (7.38%), Space: 207 MB (32.85%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    int find(vector<int> & prices,int ind,int buy, int trans, vector<vector<vector<int>>>& dp){
-        if(trans==0)return 0;
-        if(ind==prices.size())return 0;
-        if(dp[ind][buy][trans]!=-1)return dp[ind][buy][trans];
-        int price=0;
+//     int find(vector<int> & prices,int ind,int buy, int trans, vector<vector<vector<int>>>& dp){
+//         if(trans==0)return 0;
+//         if(ind==prices.size())return 0;
+//         if(dp[ind][buy][trans]!=-1)return dp[ind][buy][trans];
+//         int price=0;
         
-        if(buy){
-            price=max((-prices[ind] + find(prices,ind+1,0,trans,dp)),                                                  (find(prices,ind+1,1,trans,dp)));
-        }
-        else{
-            price=max((prices[ind] + find(prices,ind+1,1,trans-1,dp)),                                                  (find(prices,ind+1,0,trans,dp)));
-        }
-        return dp[ind][buy][trans]=price;
-    }
+//         if(buy){
+//             price=max((-prices[ind] + find(prices,ind+1,0,trans,dp)),                                                  (find(prices,ind+1,1,trans,dp)));
+//         }
+//         else{
+//             price=max((prices[ind] + find(prices,ind+1,1,trans-1,dp)),                                                  (find(prices,ind+1,0,trans,dp)));
+//         }
+//         return dp[ind][buy][trans]=price;
+//     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return find(prices,0,1,2,dp);
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=1;buy>=0;buy--){
+                for(int trans=2;trans>0;trans--){
+                    int price=0;
+                     if(buy){
+                        price=max(-prices[ind] + dp[ind+1][0][trans],                                                  dp[ind+1][1][trans]);
+                    }
+                    else{
+                        price=max(prices[ind] + dp[ind+1][1][trans-1],                                                  dp[ind+1][0][trans]);
+                    }
+                    dp[ind][buy][trans]=price;
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
