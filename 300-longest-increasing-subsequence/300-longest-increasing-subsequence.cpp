e7cdb4b18ef40fbfc79a0ca198565cf004a20bc9
ksgeu0905 <91470947+ksgeu0905@users.noreply.github.com>
2022-07-24 17:57:50 +0530

    Time: 1371 ms (7.93%), Space: 292.2 MB (9.44%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -25,20 +25,43 @@ public:
     // }
     
     
-       int lengthOfLIS(vector<int>& nums) {
+    // TABULATION
+    int lengthOfLIS(vector<int>& nums) {
            int n=nums.size();
-           vector<int> temp;
-           temp.push_back(nums[0]);
-           for(int i=1;i<n;i++){
-               if(nums[i]>temp.back())
-                   temp.push_back(nums[i]);
-               else{
-                   int ind=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
-                   temp[ind]=nums[i];
+          
+
+           vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+           for(int ind=n-1;ind>=0;ind--){
+               for(int last=ind-1;last>=-1;last--){
+                   int pick=0;
+                    int n_pick=dp[ind+1][last+1];
+                    if(last==-1 || nums[ind]>nums[last]){
+                        pick=1+dp[ind+1][ind+1];
+                    }
+                    dp[ind][last+1]=max(pick,n_pick);
                }
            }
-           return temp.size();
-               
+            return dp[0][0];   
        }
+    
+    
+    
+    // Using binary search
+    
+//        int lengthOfLIS(vector<int>& nums) {
+//            int n=nums.size();
+//            vector<int> temp;
+//            temp.push_back(nums[0]);
+//            for(int i=1;i<n;i++){
+//                if(nums[i]>temp.back())
+//                    temp.push_back(nums[i]);
+//                else{
+//                    int ind=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+//                    temp[ind]=nums[i];
+//                }
+//            }
+//            return temp.size();
+               
+//        }
 
 };
