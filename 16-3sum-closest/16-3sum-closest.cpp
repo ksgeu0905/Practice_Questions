5a62c47b212231d414c57264d64db8073687f069
ksgeu0905 <91470947+ksgeu0905@users.noreply.github.com>
2022-10-08 13:18:13 +0530

    Time: 445 ms (56.27%), Space: 16.3 MB (48.38%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        if(nums.size() < 3) return 0;
-    int closest = nums[0]+nums[1]+nums[2];
-    sort(nums.begin(), nums.end());
-    for(int first = 0 ; first < nums.size()-2 ; ++first) {
-        if(first > 0 && nums[first] == nums[first-1]) continue;
-        int second = first+1;
-        int third = nums.size()-1;            
-        while(second < third) {
-            int curSum = nums[first]+nums[second]+nums[third];
-            if(curSum == target) return curSum;
-            if(abs(target-curSum)<abs(target-closest)) {
-                closest = curSum;
-            }
-            if(curSum > target) {
-                --third;
-            } else {
-                ++second;
+        int result = 0, max_num = INT_MAX, n = nums.size();
+        sort(nums.begin(),nums.end());
+        
+        for(int i = 0;i <n;i++){
+            
+            // Continue if value at ith index is a repeating value
+            if(i != 0 && nums[i] == nums[i-1])
+                continue;
+            
+            int j = i+1;
+            int k = n-1;
+            
+            while(j<k){
+                int val = nums[i]+nums[j]+nums[k];
+                if(abs(target-val) < max_num){
+                    max_num = abs(target-val);
+                    result = val;
+                }
+                
+                if(val < target) j++;
+                else if(val > target) k--;
+                else return result;
             }
         }
-    }
-    return closest;
+        return result;
     }
 };
