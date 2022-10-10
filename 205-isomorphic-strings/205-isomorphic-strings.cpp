89886d4eb4e540ed514ba16e17baabefad57d4ce
ksgeu0905 <91470947+ksgeu0905@users.noreply.github.com>
2022-10-10 18:14:44 +0530

    Time: 16 ms (37.04%), Space: 7.2 MB (12.18%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        map<char,char>mp,mp1;
+        for(int i=0;i<s.length();i++)
+        {
+            if(mp.find(s[i])==mp.end())
+                mp[s[i]]=t[i];
+            else
+            {
+                if(mp[s[i]]!=t[i])
+                    return false;
+            }
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            if(mp1.find(t[i])==mp1.end())
+                mp1[t[i]]=s[i];
+            else
+            {
+                if(mp1[t[i]]!=s[i])
+                    return false;
+            }
+        }
+        return true;
+    }
+};
