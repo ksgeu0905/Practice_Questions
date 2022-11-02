2658ccc0f59440ef58e7396760722a11568d6f17
ksgeu0905 <91470947+ksgeu0905@users.noreply.github.com>
2022-11-02 10:48:05 +0530

    Time: 3 ms (35.67%), Space: 6.2 MB (20.85%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    int tribonacci(int n) {
-        int x=0,y=1,z=1;
+    int find(int n,vector<int>&dp){
         if(n==0)return 0;
-        if(n==1)return 1;
-        if(n==2)return 1;
-        n=n-2;
-        int curr;
-        while(n--){
-            curr=x+y+z;
-            x=y;
-            y=z;
-            z=curr;
-        }
-        return curr;
-        
+        if(n==1 || n==2)return 1;
+        if(dp[n]!=-1)return dp[n];
+        return dp[n]=(find(n-1,dp)+find(n-2,dp)+find(n-3,dp));
+    }
+    int tribonacci(int n) {
+        vector<int>dp(n+1,-1);
+
+        return find(n,dp);
     }
 };
