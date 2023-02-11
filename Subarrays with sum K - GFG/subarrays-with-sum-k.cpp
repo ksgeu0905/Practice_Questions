8b222eed8b9739ba9c69ec9f11ea354ac2b1a13d
Kartikya Singhal <91470947+ksgeu0905@users.noreply.github.com>
2023-02-11 13:31:42 +0530

    Added solution - LeetHub

---
+++ Subarrays with sum K - GFG/subarrays-with-sum-k.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int findSubArraySum(int arr[], int N, int k)
+    {
+        map<int,int>mp;
+        mp[0]=1;
+        int sum=0,ans=0;
+        for(int i=0;i<N;i++){
+            sum+=arr[i];
+            if(mp.find(sum-k)!=mp.end()){
+                ans+=mp[sum-k];
+            }
+            mp[sum]++;
+        }
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        int Arr[N];
+        for(int i=0;i<N;i++)    
+            cin>>Arr[i];
+        int k;
+        cin>>k;
+        Solution obj;
+        cout<<obj.findSubArraySum(Arr, N, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
